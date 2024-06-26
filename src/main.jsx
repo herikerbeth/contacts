32454d7a4ca6ced381aec2b68757a270362f87f1
herikerbeth <herik.docarmo@hotmail.com>
2024-06-26 15:15:11 -0300

    add a loader to the contact page

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import {
 } from "react-router-dom";
 import './index.css';
 import ErrorPage from "./error-page";
-import Contact from './routes/contact';
-
+import Contact, {
+  loader as contactLoader,
+} from './routes/contact';
 import Root, { 
   loader as rootLoader, 
   action as rootAction,
@@ -24,6 +25,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },

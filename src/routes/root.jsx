a52ad7200fc08eae8f5bd955abd81a8f1b96d1fc
herikerbeth <herik.docarmo@hotmail.com>
2024-06-27 17:07:38 -0300

    use a NavLink in sidebar

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,6 @@
 import { 
     Outlet, 
+    NavLink,
     Link, 
     useLoaderData,
     Form,
@@ -51,7 +52,17 @@ export default function Root() {
                         <ul>
                             {contacts.map((contact) => (
                                 <li key={contact.id}>
-                                    <Link to={`contacts/${contact.id}`}>
+
+                                    <NavLink
+                                      to={`contacts/${contact.id}`}
+                                      className={({ isActive, isPending }) =>
+                                        isActive
+                                          ? "active"
+                                          : isPending
+                                          ? "pending"
+                                          : ""
+                                      }
+                                    >
                                         {contact.first || contact.last ? (
                                             <>
                                                 {contact.first} {contact.last}
@@ -60,7 +71,7 @@ export default function Root() {
                                             <i>No name</i>
                                         )}{" "}
                                         {contact.favorite && <span>★</span>}
-                                    </Link>
+                                    </NavLink>
                                 </li>
                             ))}
                         </ul>

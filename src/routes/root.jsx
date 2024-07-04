b9546eb0f06f8eb2c0233460fb4662722d1f993d
herikerbeth <herik.docarmo@hotmail.com>
2024-07-04 11:02:09 -0300

    use useSubmit

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -7,6 +7,7 @@ import {
     Form,
     redirect,
     useNavigation,
+    useSubmit,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -26,6 +27,7 @@ export default function Root() {
     const { contacts, q } = useLoaderData();
     const [query, setQuery] = useState(q);
     const navigation = useNavigation();
+    const submit = useSubmit();
 
     useEffect(() => {
         setQuery(q);
@@ -45,7 +47,7 @@ export default function Root() {
                           name="q"
                           value={query}
                           onChange={(e) => {
-                            setQuery(e.target.value);
+                            submit(e.target.form);
                           }}
                         />
                         <div 
